969296329c0ebf534312871c774761c3913581ee
Shamsutdinov Rishat <31825566+iShamSLam@users.noreply.github.com>
2020-12-25 10:38:48 +0300

    *

--- gltronred-fp-2020-tasks-62a0a839d0e5/test/Test3.hs
+++ gltronred-fp-2020-tasks-62a0a839d0e5/test/Test3.hs
@@ -30,6 +30,9 @@ test18 :: TestTree
 test18 = testGroup "P18"
   [ testCase "prob18 17 == T" $ prob18 17 @?= True
   , testCase "prob18 1 == F" $ prob18 1 @?= False
+  , testCase "prob18 479001599 == T" $ prob18 479001599 @?= True
+  , testCase "prob18 2971215073 == T" $ prob18 2971215073 @?= True
+  , testCase "prob18 2971215073 == F" $ prob18 8971215073 @?= False
   ]
 
 test19 :: TestTree
@@ -45,6 +48,9 @@ test20 = testGroup "P20"
   [ testCase "prob20 6 == T" $ prob20 6 @?= True
   , testCase "prob20 8 == F" $ prob20 8 @?= False
   , testCase "prob20 1 == F" $ prob20 1 @?= False
+  , testCase "prob20 33550336 == T" $ prob20 33550336 @?= True
+  , testCase "prob20 8589869056 == T" $ prob20 8589869056 @?= True
+  , testCase "prob20 10 ^ 10 == F" $ prob20 (10 ^ 10) @?= False
   ]
 
 test21 :: TestTree
@@ -52,16 +58,35 @@ test21 = testGroup "P21"
   [ testCase "prob21 6 == [1,2,3,6]" $ prob21 6 @?= [1,2,3,6]
   , testCase "prob21 7 == [1,7]" $ prob21 7 @?= [1,7]
   , testCase "prob21 1 == [1]" $ prob21 1 @?= [1]
+  , testCase "prob21 1048576 == [1, 2, 4, .. , 524288, 1048576]" $ 
+    prob21 1048576 @?= [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
+                        32768, 65536, 131072, 262144, 524288, 1048576]
+  , testCase "prob21 9246432350 == [1, 2, 5, .. , 4623216175, 9246432350]" $ 
+    prob21 9246432350 @?= [1, 2, 5, 10, 25, 50, 2393, 4786, 11965, 23930, 59825, 77279, 119650,
+                           154558, 386395, 772790, 1931975, 3863950, 184928647, 369857294, 924643235, 
+                           1849286470, 4623216175, 9246432350]
   ]
 
 test22 :: TestTree
 test22 = testGroup "P22"
-  [ testCase "prob22 \"part iii izi\" == 6" $
+  [ testCase "prob22 \"parti iii izi\" == 6" $
     prob22 "parti iii izi" @?= 6
-  , testCase "prob22 \"I have no letters\" == 1" $
+  , testCase "prob22 \"I have no letters\" == 0" $
     prob22 "I have no letters" @?= 0
-  , testCase "prob22 \"no letters\" == 1 " $
+  , testCase "prob22 \"no letters\" == 0 " $
     prob22 "no letters" @?= 0
+  , testCase "prob22 \"\" == 0 " $
+    prob22 "" @?= 0
+  , testCase "prob22 \"i\" == 1 " $
+    prob22 "i" @?= 1
+  , testCase "prob22 \"iii\" == 3 " $
+    prob22 "iii" @?= 3
+  , testCase "prob22 \"iii iii\" == 9 " $
+    prob22 "iii iii" @?= 9
+  , testCase "prob22 \"iii\n\tiii\n\ti0i\" == 18 " $
+    prob22 "iii\n\tiii\n\ti0i" @?= 18
+  , testCase "prob22 \"\0105\0105 \0105\0105i\" == 6 " $
+    prob22 "\0105\0105 \0105\0105i" @?= 6
   ]
 
 test23 :: TestTree
@@ -82,20 +107,33 @@ test24 = testGroup "P24"
   , testCase "prob24 2 == F" $ prob24 2 @?= False
   , testCase "prob24 6 == T" $ prob24 6 @?= True
   , testCase "prob24 15 == T" $ prob24 15 @?= True
+  , testCase "prob24 666 == T" $ prob24 666 @?= True
+  , testCase "prob24 1830 == T" $ prob24 1830 @?= True
+  , testCase "prob24 500500 == T" $ prob24 500500 @?= True
+  , testCase "prob24 200010000 == T" $ prob24 200010000 @?= True
+  , testCase "prob24 50000 * 50001 `div` 2 == T" $ prob24 (50000 * 50001 `div` 2) @?= True
   ]
 
 test25 :: TestTree
 test25 = testGroup "P25"
   [ testCase "prob25 9 == T" $ prob25 9 @?= True
+  , testCase "prob25 0 T" $ prob25 0 @?= True
   , testCase "prob25 10 == F" $ prob25 10 @?= False
   , testCase "prob25 101 == T" $ prob25 101 @?= True
+  , testCase "prob25 101..101 == T" $ prob25 1010101010101 @?= True
+  , testCase "prob25 906609 == T" $ prob25 906609 @?= True
   ]
 
 test26 :: TestTree
 test26 = testGroup "P26"
-  [ testCase "prob26 1 2 == F" $ prob26 1 2 @?= False
-  , testCase "prob26 6 6 == T" $ prob26 6 6 @?= True
-  , testCase "prob26 220 284 == T" $ prob26 220 284 @?= True
+  [ testCase "prob26 1 2 == F"         $ prob26 1     2     @?= False
+  , testCase "prob26 6 6 == T"         $ prob26 6     6     @?= True
+  , testCase "prob26 220 284 == T"     $ prob26 220   284   @?= True
+  , testCase "prob26 1184 1210 == T"   $ prob26 1184  1210  @?= True
+  , testCase "prob26 2620 2924 == T"   $ prob26 2620  2924  @?= True
+  , testCase "prob26 10744 10856 == T" $ prob26 10744 10856 @?= True
+  , testCase "prob26 10744 10856 == T" $ prob26 69615 87633 @?= True
+  , testCase "prob26 79750 88730 == T" $ prob26 79750 88730 @?= True
   ]
 
 test27 :: TestTree
@@ -108,6 +146,12 @@ test27 = testGroup "P27"
     prob27 4 [2,1,2] @?= Just (2,2)
   , testCase "prob27 4 [1,2,1] == N" $
     prob27 4 [1,2,1] @?= Nothing
+  , testCase "prob27 501 (take 500 [1..]) == (1, 500)" $
+    prob27 501 (take 500 [1..]) @?= Just (1, 500)
+  , testCase "prob27 1999 (take 1000 [1..]) == (999, 1000)" $
+    prob27 1999 (take 1000 [1..]) @?= Just (999, 1000)
+  , testCase "prob27 1999 (take 1000 [1..]) == (999, 1000)" $
+    prob27 1999 (take 1000 [1..]) @?= Just (999, 1000)
   ]
 
 test28 :: TestTree
@@ -116,27 +160,112 @@ test28 = testGroup "P28"
     prob28 5 [3,1,1,1,2] @?= Just (2,1,1,1)
   , testCase "prob28 5 [3,2,1,1,5] == N" $
     prob28 5 [3,2,1,1,5] @?= Nothing
+  , testCase "prob28 1994 (take 500 [1..]) == (500, 499, 498, 497)" $
+    prob28 1994 (take 500 [1..]) @?= Just (500, 499, 498, 497)
+  , testCase "prob28 3994 (take 1000 [1..]) == (1000, 999, 998, 997)" $
+    prob28 3994 (take 1000 [1..]) @?= Just (1000, 999, 998, 997)
   ]
 
 test29 :: TestTree
 test29 = testGroup "P29"
-  [ testCase "prob29 1 == 9" $ prob29 1 @?= 9
+  [ testCase "prob29 1 == 9"        $ prob29 1 @?= 9
+  , testCase "prob29 2 == 9009"     $ prob29 2 @?= 9009
+  , testCase "prob29 3 == 906609"   $ prob29 3 @?= 906609
   ]
 
 test30 :: TestTree
 test30 = testGroup "P30"
-  [ testCase "prob30 4 == 6" $ prob30 4 @?= 6
-  , testCase "prob30 5 == 28" $ prob30 5 @?= 28
+  [ testCase "prob30 -1 == 0"   $ prob30 (-1) @?= 0
+  , testCase "prob30 0 == 0"    $ prob30 0    @?= 0
+  , testCase "prob30 1 == 1"    $ prob30 1    @?= 1
+  , testCase "prob30 4 == 6"    $ prob30 4    @?= 6
+  , testCase "prob30 5 == 28"   $ prob30 5    @?= 28
+  , testCase "prob30 7 == 66"   $ prob30 7    @?= 36
+  , testCase "prob30 16 == 120" $ prob30 16   @?= 120
+  , testCase "prob30 20 == 528" $ prob30 20   @?= 528
+  , testCase "prob30 24 == 630" $ prob30 24   @?= 630
+
+  , testCase "triangulars"      $
+    (all (== True) $ zipWith (==) triangularNumbers finiteTriangularNumbers) @?= True
+
+  , testCase "divisors-of-zero" $ getAllUnorderedDivisors 0 @?= []
+  , testCase "divisors-of-one"  $ getAllUnorderedDivisors 1 @?= [1]
   ]
+
+finiteTriangularNumbers :: [Integer]
+finiteTriangularNumbers = 
+    [
+        0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 
+        153, 171, 190, 210, 231, 253, 276, 300, 325, 351, 378, 406, 435, 465, 
+        496, 528, 561, 595, 630, 666, 703, 741, 780, 820, 861, 903, 946, 990, 
+        1035, 1081, 1128, 1176, 1225, 1275, 1326, 1378, 1431
+    ]
 
 test31 :: TestTree
 test31 = testGroup "P31"
-  [ testCase "prob31 250 == 0" $ prob31 250 @?= 0
-  , testCase "prob31 300 == 504" $ prob31 300 @?= 504
+  [ testCase "prob31 250 == 0"        $ prob31 250   @?= 0
+  , testCase "prob31 300 == 504"      $ prob31 300   @?= 504
+  , testCase "prob31 6368 == 19026"   $ prob31 6368  @?= 19026
+  , testCase "prob31 10000 == 31626"  $ prob31 10000 @?= 31626
+  , testCase "prob31 20000 == 115818" $ prob31 20000 @?= 115818
+  , testCase "prob31 70000 == 249738" $ prob31 70000 @?= 249738
   ]
 
 test32 :: TestTree
 test32 = testGroup "P32"
   [ testCase "prob32 [2,3,5] 10 == [5+5, 5+3+2, 3+3+2+2, 2+2+2+2+2]" $
     prob32 [2,3,5] 10 @?= [[5,5],[5,3,2],[3,3,2,2],[2,2,2,2,2]]
-  ]
+  , testCase "prob32 [2,3,5] 50 == [5+..+5, .. ,2+..+2]" $
+    prob32 [2,3,5] 50 @?= [
+        [5,5,5,5,5,5,5,5,5,5],
+        [5,5,5,5,5,5,5,5,5,3,2],
+        [5,5,5,5,5,5,5,5,3,3,2,2],
+        [5,5,5,5,5,5,5,5,2,2,2,2,2],
+        [5,5,5,5,5,5,5,3,3,3,3,3],
+        [5,5,5,5,5,5,5,3,3,3,2,2,2],
+        [5,5,5,5,5,5,5,3,2,2,2,2,2,2],
+        [5,5,5,5,5,5,3,3,3,3,3,3,2],
+        [5,5,5,5,5,5,3,3,3,3,2,2,2,2],
+        [5,5,5,5,5,5,3,3,2,2,2,2,2,2,2],
+        [5,5,5,5,5,5,2,2,2,2,2,2,2,2,2,2],
+        [5,5,5,5,5,3,3,3,3,3,3,3,2,2],
+        [5,5,5,5,5,3,3,3,3,3,2,2,2,2,2],
+        [5,5,5,5,5,3,3,3,2,2,2,2,2,2,2,2],
+        [5,5,5,5,5,3,2,2,2,2,2,2,2,2,2,2,2],
+        [5,5,5,5,3,3,3,3,3,3,3,3,3,3],
+        [5,5,5,5,3,3,3,3,3,3,3,3,2,2,2],
+        [5,5,5,5,3,3,3,3,3,3,2,2,2,2,2,2],
+        [5,5,5,5,3,3,3,3,2,2,2,2,2,2,2,2,2],
+        [5,5,5,5,3,3,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,5,5,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,5,5,3,3,3,3,3,3,3,3,3,3,3,2],
+        [5,5,5,3,3,3,3,3,3,3,3,3,2,2,2,2],
+        [5,5,5,3,3,3,3,3,3,3,2,2,2,2,2,2,2],
+        [5,5,5,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2],
+        [5,5,5,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,5,5,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,5,3,3,3,3,3,3,3,3,3,3,3,3,2,2],
+        [5,5,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2],
+        [5,5,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2],
+        [5,5,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2],
+        [5,5,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,5,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+        [5,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2],
+        [5,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2],
+        [5,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2],
+        [5,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [5,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2],
+        [3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2],
+        [3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2],
+        [3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2],
+        [3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]]
+   ]
